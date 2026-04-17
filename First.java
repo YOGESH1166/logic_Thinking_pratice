7e4a7c825db4e148f18f6a85eb62411530d883e8
YOGESH <yogeshsekar8211@gmail.com>
2026-04-17 15:09:05 +0530

    level 1

--- First.java
+++ First.java
@@ -1,5 +1,5 @@
 class First{
   public static void main (String[]args){
-    System.out.println("yogesh");
+    System.out.println("yogii");
   }
 }
---
+++ sample.py
@@ -0,0 +1,33 @@
+"""num = int(input("enter the number:"))
+if num%2==1:
+  print("num is odd")
+else:
+  print("num is even")"""
+
+
+'''num1 = int(input("ENTER NUMBER VALUE:"))
+num4 = int(input("ENTER NUMBER VALUE:"))
+num3 = int(input("ENTER NUMBER VALUE:"))
+if num1>num4 and num1>num3:
+  print("largest number is num1 ")
+elif num1<num4 and num4>num3:
+  print("largest number num4")
+else:
+  print("num3 is largest ")'''
+
+
+
+'''n = int(input("enter the number:"))
+if n>0:
+  print("n is postive ")
+elif n==0:
+  print("n value is zero")
+else:
+  print("n is negative")'''
+
+
+'''number = int(input("number:"))
+for i in range(1,number+1):
+  print(i)'''
+
+
